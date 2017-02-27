77b9f546d25714ba8c0e5caf3a71e27516911e99
y-hryk <dev.hy630823@gmail.com>
2017-02-27 15:31:07 +0900

    update

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
+
   def home
+    
   end
 
   def help
@@ -12,4 +14,5 @@ class StaticPagesController < ApplicationController
   def contact
   	
   end
+  
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+	#ページごとのタイトルを返す
+	def full_title(page_title = '')
+		base_title = "Ruby on Rails Tutorial Sample App"
+
+		if base_title.empty?
+			return base_title
+		else
+			return page_title + " | " + base_title
+		end
+
+	end
+
+
 end
