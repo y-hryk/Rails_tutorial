74334a9f28cf0239ac68ab24ade57951f4cc42a4
y-hryk <dev.hy630823@gmail.com>
2017-03-06 17:11:10 +0900

    email validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -60,4 +60,18 @@ class UserTest < ActiveSupport::TestCase
 
   end
 
+  test "email addresses should be unique" do
+  	# 'dup' データを複製する関数
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "email addresses should be saved as lower-case" do
+  	mixed_case_email = "Foo@ExAMPle.CoM"
+  	@user.email = mixed_case_email
+  	@user.save
+  	assert_equal mixed_case_email.downcase, @user.reload.email
+  end
 end
