fe4b15842662f5cb9e651aca909e1986fa9f957d
y-hryk <dev.hy630823@gmail.com>
2017-03-08 19:08:32 +0900

    add signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,31 @@
 class UsersController < ApplicationController
 
   def show
-  	@user = User.find(params[:id])
+  		@user = User.find(params[:id])
+  	  	logger.debug { @user.inspect }
+  	  	logger.debug { ">>> #{params.inspect}" }
   end
 
   def new
+  	@user = User.new
   end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+
+  	else
+  		render 'new'
+  	end
+  end
+
+  private
+
+    def user_params
+
+  		logger.debug { ">>> inputParams  #{params.inspect}"  }
+    	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+
+    end
   
 end
